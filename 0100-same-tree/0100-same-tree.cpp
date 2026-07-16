683c114314ab5cd1f7c1b18cbd6d0cf9772362c2
Akshay Gupta <99547720+Akshay-Gupt-a@users.noreply.github.com>
2026-07-16 21:09:45 +0530

    Time: 0 ms (100%), Space: 12.8 MB (78.19%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -15,20 +15,15 @@ public:
         if(p==NULL && q==NULL){
             return true;
         }
-        if((p==NULL && q!=NULL) || (p!=NULL & q==NULL) ){
+        if((p==NULL  || q==NULL) ){
             return false;
         }
         if(p->val!=q->val){
             return false;
         }
         bool left =isSameTree(p->left,q->left);
-        if(!left){
-            return false;
-        }
         bool right = isSameTree(p->right,q->right);
-        if(!right){
-            return false;
-        }
-        return true;
+       
+        return (left && right);
     }
 };
