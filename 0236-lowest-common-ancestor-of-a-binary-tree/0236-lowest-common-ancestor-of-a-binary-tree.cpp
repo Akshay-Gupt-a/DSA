ca383fa7c71f4557c2374e22605568853cbe143b
Akshay Gupta <99547720+Akshay-Gupt-a@users.noreply.github.com>
2026-07-20 20:36:25 +0530

    Time: 70 ms (18.36%), Space: 44.1 MB (21.53%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -12,7 +12,7 @@ public:
  
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(!root) return NULL;
-        if(root==p || root== q){
+        if(root == p || root == q){
             return root;
         }
        
@@ -21,14 +21,7 @@ public:
         if(left!=NULL && right!=NULL){
             return root;
         }
-        if(left==NULL || right== NULL){
-            if(left!=NULL){
-                return left;
-            }
-            else{
-                return right;
-            }
-        }
-        return NULL;
+      
+        return left ? left : right;
     }
 };
